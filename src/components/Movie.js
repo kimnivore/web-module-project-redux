5e99ce3560747b455f45fc242724932231e41895
Kim Nguyen <kimnivore@gmail.com>
2022-01-18 15:14:17 -0800

    favorites actions built out

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { deleteMovie } from './../actions/movieActions';
-
+import { addFavorite } from './../actions/favoritesActions';
+ 
 const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const { movies, deleteMovie, displayFavorites } = props;
+    const { movies, deleteMovie, displayFavorites, addFavorite} = props;
     const movie = movies.find(movie=>movie.id===Number(id));
     
     const handleDelete = () => {
         deleteMovie(movie.id);
         push('/movies');
+    }
+
+    const handleAddFavorite = () => {
+        addFavorite(movie);
     }
 
     return(<div className="modal-page col">
@@ -44,7 +49,7 @@ const Movie = (props) => {
                         </section>
                         
                         <section>
-                            <span className="m-2 btn btn-dark">Favorite</span>
+                            {displayFavorites && <span className="m-2 btn btn-dark" onClick={handleAddFavorite}>Favorite</span>}
                             <span className="delete" onClick={handleDelete} ><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
@@ -61,4 +66,4 @@ const mapStateToProps = (state) => {
     });
 }
 
-export default connect(mapStateToProps, {deleteMovie})(Movie);
+export default connect(mapStateToProps, {deleteMovie, addFavorite})(Movie);
--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -1,15 +1,27 @@
+import { TOGGLE_FAVORITES, ADD_FAVORITE, REMOVE_FAVORITE } from './../actions/favoritesActions';
+
 const initialState = {
-    favorites: [{title: "Kim's movie", id: 1}, {title: "Keane's movie", id: 2}],
+    favorites: [],
     displayFavorites: true
 };
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        // case:
-        //     return {
-
-        //     }
-
+        case TOGGLE_FAVORITES:
+            return {
+                ...state,
+                displayFavorites: !state.displayFavorites
+            };
+        case ADD_FAVORITE:
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            }
+         case REMOVE_FAVORITE:
+            return {
+                ...state,
+                favorites: state.favorites.filter(item => (item.id !== action.payload))
+            }
         default:
             return state;
     }
